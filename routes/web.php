2b2cc13b5a9a3fe4e7f7c4160ce630d5a45fe8f3
TheOnlySmartBoy <o2jose43@gmail.com>
2021-01-20 17:54:24 +0300

    Updated web routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,27 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//super admin route group
+Route::group(['prefix' => 'super'], function () {
+    Route::get('/', [App\Http\Controllers\SuperAdminController::class,'index'])->name('superadminhome');
+
+});
+
+//Admin route group
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', [App\Http\Controllers\AdminController::class,'index'])->name('adminhome');
+
+});
+
+//Vendor route group
+Route::group(['prefix' => 'vendor'], function () {
+    Route::get('/', [App\Http\Controllers\VendorController::class,'index'])->name('vendorhome');
+
+});
+
+//User route group
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [App\Http\Controllers\UserController::class,'index'])->name('userhome');
+
+});
