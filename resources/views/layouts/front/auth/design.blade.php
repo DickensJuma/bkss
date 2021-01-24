119ce00b848a4baf7af86cdf46d450bac740d179
TheOnlySmartBoy <o2jose43@gmail.com>
2021-01-24 18:42:33 +0300

    Added admin dashboard template

--- resources/views/layouts/front/auth/design.blade.php
+++ resources/views/layouts/front/auth/design.blade.php
@@ -8,21 +8,21 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{asset('front/plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{asset('back/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{asset('front/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('back/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('front/dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="{{asset('back/dist/css/adminlte.min.css')}}">
 </head>
 <body class="hold-transition login-page">
 @include('layouts.front.header')
 <!--page content section-->
 @yield('content')
 <!-- jQuery -->
-<script src="{{asset('front/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('back/plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{asset('front/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('back/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset('front/dist/js/adminlte.min.js')}}"></script>
+<script src="{{asset('back/dist/js/adminlte.min.js')}}"></script>
 </body>
 </html>
