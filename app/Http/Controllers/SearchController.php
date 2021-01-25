e8131d9c854636da830fc31061670ad05d9c142b
TheOnlySmartBoy <o2jose43@gmail.com>
2021-01-25 19:10:01 +0300

    Testing search module

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Property;
 
 class SearchController extends Controller
 {
    public function search(Request $request){
     $data = $request->all();
-    dd($data);
-    $destinations = Property::where(['location' => $restaurant_id])->where([])->get();
-            return view('vendor.products.index')->with(compact('meals'));
+    $destinations = Property::where(['location' => $request->destination])->where(['capacity'=>$request->adult])->get();
+    if($destinations != null){
+        dd($destinations);
+    }else{
+        echo "Nothing found";
+        
+    }
+            
 
 
    }
