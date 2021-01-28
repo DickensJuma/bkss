e09aae7448a3054d8101f21425ee8e102ef923cb
TheOnlySmartBoy <o2jose43@gmail.com>
2021-01-28 20:07:47 +0300

    Upgrading booking logic

--- database/migrations/2021_01_28_125355_create_images_table.php
+++ database/migrations/2021_01_28_125355_create_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePropertiesTable extends Migration
+class CreateImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,8 @@ class CreatePropertiesTable extends Migration
      */
     public function up()
     {
-        Schema::create('properties', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('address');
-            $table->tinyInteger('owner');
-            $table->tinyInteger('location');
-            $table->string('cover_image');
-            $table->string('logo')->nullable()->default('text');
             $table->timestamps();
         });
     }
@@ -32,6 +26,6 @@ class CreatePropertiesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('properties');
+        Schema::dropIfExists('images');
     }
 }
