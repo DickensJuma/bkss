74054baed93d1f5200d67a32c5120011b07bb35b
TheOnlySmartBoy <o2jose43@gmail.com>
2021-03-04 16:20:24 +0300

    Begunadding service charge selector to levies

--- resources/views/property/levy.blade.php
+++ resources/views/property/levy.blade.php
@@ -51,19 +51,41 @@
                                                 <td>{{ $levy->percentage }}%</td>
                                             </tr>
                                             @endforeach
+                                            <tr>
+                                                <td>Property service Charge</td>
+                                                <td><select name="servicecharge" id="servicecharge">
+                                                    <option value="">Select</option>
+                                                    <option value="1">1%</option>
+                                                    <option value="2">2%</option>
+                                                    <option value="3">3%</option>
+                                                    <option value="4">4%</option>
+                                                    <option value="5">5%</option>
+                                                    <option value="6">6%</option>
+                                                    <option value="7">7%</option>
+                                                    <option value="8">8%</option>
+                                                    <option value="9">9%</option>
+                                                    <option value="1">1%</option>
+                                                    <option value="10">10%</option>
+                                                    <option value="11">11%</option>
+                                                    <option value="12">12%</option>
+                                                    <option value="13">13%</option>
+                                                    <option value="14">14%</option>
+                                                    <option value="15">15%</option>
+                                                </select>
+                                            </td>
+                                            </tr>
                                         </tbody>
                                     </table>
-                                    @foreach ($levies as $levy)
-                                    
-                                        
-                                    @endforeach
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                
             </section>
     </div>
+    <script>
+        
+
+    </script>
     
 @endsection
