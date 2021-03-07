479ebee62d47938eb38734ffa282f5ec4324a73e
TheOnlySmartBoy <o2jose43@gmail.com>
2021-03-07 23:31:29 +0300

    Update2

--- resources/views/layouts/front/design.blade.php
+++ resources/views/layouts/front/design.blade.php
@@ -30,7 +30,7 @@
         <!-- Theme CSS -->
         <link rel="stylesheet" href="{{asset('front/assets/css/theme.min.css')}}">
         <link rel="stylesheet" href="{{asset('front/assets/css/style.css')}}">
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="{{asset('back/plugins/jquery/jquery.min.js')}}"></script>
         <!-- bs-custom-file-input -->
         <script src="{{asset('back/plugins/bs-custom-file-input/bs-custom-file-input.min.js')}}"></script>
 
