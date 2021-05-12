39539f0aa6f9061f46fa8fca91eaebbc5c826550
TheOnlySmartBoy <o2jose43@gmail.com>
2021-05-12 22:55:59 +0300

    Minor update

--- resources/views/property/profile/index.blade.php
+++ resources/views/property/profile/index.blade.php
@@ -23,37 +23,48 @@
                 <div class="row">
                     <div class="col-12">
                         <p>This info can be shown to potential guests on our website and is an opportunity to help your property stand out.</p>
-                        <form action="{{ route('profile.add') }}" method="POST">
-                            <div class="form-group row">
+                        <form action="{{ route('profile.add', $property_id) }}" method="POST">
+                            <div class="form-group">
                                 <p>Pick "Host profile" if you personally manage your property or "Company profile" if you're a business.</p>
-                                <div class="col-md-6">
-                                    <input type="radio" name="host" value="host" class="form-control">
+                                <div class="form-check form-check-inline">
+                                    <input type="radio" name="type" value="1" class="form-check">
                                     <label class="label-check" for="host">Host Profile</label>
                                 </div>
-                                <div class="col-md-6">
-                                    <input type="radio" name="company" value="company" class="form-control">
+                                <div class="form-check form-check-inline">
+                                    <input type="radio" name="type" value="2" class="form-check">
                                     <label class="label-check" for="company">Company Profile</label>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <label for="">Host name</label>
-                                <input type="text" name="host_name">
+                            <div class="form-group row">
+                                <div class="col-xs-4">
+                                    <label for="">Host name</label>
+                                    <input type="text" name="host_name" class="form-control">
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="">About the Property</label><br>
-                                <small>What makes your place unique and how can you help guests feel more welcome? Think about decor, amenities and special features. Don't add House Rules here, this goes under the Policies section.
-                                </small>
-                                <textarea name="about" cols="30" rows="5" class="form-control"></textarea>
+                            <div class="form-group row">
+                                <div class="col-xs-4">
+                                    <label for="">About the Property</label><br>
+                                    <small class="help-block">What makes your place unique and how can you help guests feel more welcome? 
+                                        Think about decor, amenities and special features. Don't add House Rules here, <br>
+                                        goes under the Policies section.
+                                    </small>
+                                    <textarea name="about" cols="30" rows="5" class="form-control"></textarea>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="">About Host</label><br>
-                                <small>Help guests feel at ease and excited about their trip with a short welcome message. What do you (or your team) enjoy about hosting? Share personal interests or hobbies.</small>
-                                <textarea name="about_host" id="about_host" cols="30" rows="5" class="form-control"></textarea>
+                            <div class="form-group row">
+                                <div class="col-xs-4">
+                                    <label for="">About Host</label><br>
+                                    <small class="help-block">Help guests feel at ease and excited about their trip with a short welcome 
+                                        message. What do you (or your team) enjoy about hosting? Share personal interests or <br> hobbies.</small>
+                                    <textarea name="about_host" id="about_host" cols="30" rows="5" class="form-control"></textarea>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="">About the neighborhood</label><br>
-                                <small>What's good to know about the local area and attractions? You can include points of interest like museums and famous landmarks in the "What's nearby" settings.</small>
-                                <textarea name="neighborhood" id="neighborhood" cols="30" rows="5" class="form-control"></textarea>
+                            <div class="form-group row">
+                                <div class="col-xs-4">
+                                    <label for="">About the neighborhood</label><br>
+                                    <small class="help-block">What's good to know about the local area and attractions? You can include points of interest like museums and famous landmarks in the "What's nearby" settings.</small>
+                                    <textarea name="neighborhood" id="neighborhood" cols="30" rows="5" class="form-control"></textarea>
+                                </div>
                             </div>
                             <div class="divider">
                                 <h3>About Your Property</h3>
@@ -70,8 +81,8 @@
                                                         <?php echo $month_drop_down ?>
                                                     </select>
                                                 </div>
-                                                <div class="col-md-6">
-                                                    <input type="text" class="form-control inline yearpicker" id="renting_year" name="renting_year" value="2015" placeholder="e.g. 1983">
+                                                <div class="col-md-4">
+                                                    <input type="text" class="form-control inline yearpicker" id="renting_year" name="renting_year" value="{{ now()->year }}" placeholder="e.g. 1983">
                                                 </div>
                                             </div>
                                         </div>
@@ -86,8 +97,8 @@
                                                         <?php echo $month_drop_down1 ?>
                                                     </select>
                                                 </div>
-                                                <div class="col-md-6">
-                                                    <input type="text" class="form-control inline yearpicker" id="rennovation_year" name="renting_year" value="2015" >
+                                                <div class="col-md-4">
+                                                    <input type="text" class="form-control inline yearpicker" id="rennovation_year" name="renting_year" value="{{ now()->year }}" >
                                                 </div>
                                             </div>
                                         </div>
