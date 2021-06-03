14aee935faa9f5ae2e9b7f2194fa589e44d1cfd1
Pato <nyatindopatrick@gmail.com>
2021-06-03 18:59:57 +0300

    implement gallery layout and search bar

--- resources/views/property/room/view.blade.php
+++ resources/views/property/room/view.blade.php
@@ -5,44 +5,103 @@
         <div class="card-header">
             <h5>{{ $hotel->name }} <a href=""><i class="far fa-heart"></i></a>
                 <a href=""><i class="fas fa-share-alt"></i></a>
-            <a href="" class="btn btn-primary btn-md">Reserve</a></h5>
-            <span class="text-warning" data-toggle="tooltip" data-placement="left" title="This star rating is provided to Book sasa by the property and is usually determined by an official hotel rating organization or another third party.">
+                <a href="" class="btn btn-primary btn-md">Reserve</a>
+            </h5>
+            <span class="text-warning" data-toggle="tooltip" data-placement="left"
+                title="This star rating is provided to Book sasa by the property and is usually determined by an official hotel rating organization or another third party.">
                 @for ($star =0 ; $star < $hotel->rating ; $star++)
-                &#9734;
-                @endfor
+                    &#9734;
+                    @endfor
             </span>
-            <i class="fas fa-thumbs-up text-warning" data-toggle="tooltip" data-placement="left" title="This is a Preferred Partner property. It's committed to giving guests positive experiences with its excellent service and great value. This property might pay Book sasa a little more to be in this Program"></i>
-            <i class="fas fa-map-pin" data-toggle="tooltip" data-placement="left" title="After booking, all of the propertyâ€™s details, including telephone and address, are sent to you as part of your booking confirmation and your account." >{{ $hotel->address2 }}</i>
+            <i class="fas fa-thumbs-up text-warning" data-toggle="tooltip" data-placement="left"
+                title="This is a Preferred Partner property. It's committed to giving guests positive experiences with its excellent service and great value. This property might pay Book sasa a little more to be in this Program"></i>
+            <i class="fas fa-map-pin" data-toggle="tooltip" data-placement="left"
+                title="After booking, all of the propertyâ€™s details, including telephone and address, are sent to you as part of your booking confirmation and your account.">{{ $hotel->address2 }}</i>
         </div>
-        <div class="card-body">
-            <div class="portfolio-area">
-                @foreach ($images->chunk(4) as $key=>$image)
-                <div class="row">
-                    @foreach ($image as $item)
-                    @if ($key===0)
-                    <div class="col-md-3">
-                        <div>
-                            <span class="image-block block2">
-                            <a class="image-zoom" href="{{ asset('uploads/property/large/'.$item->path) }}" rel="prettyPhoto[gallery]">							
-                                    <img src="{{ asset('uploads/property/small/'.$item->path) }}" class="img-responsive" alt="Gallery"></a>
-                        </span>
-                        </div>
-                    </div>                        
-                    @else
-                    <div class="col-md-2">
-                        <div style="display: block">
-                            <span class="image-block block2">
-                            <a class="image-zoom" href="{{ asset('uploads/property/large/'.$item->path) }}" rel="prettyPhoto[gallery]">							
-                                    <img src="{{ asset('uploads/property/small/'.$item->path) }}" class="img-responsive" alt="Gallery"></a>
+
+        <div class="gallery-card card-body">
+            <div class="pics-area">
+                @foreach ($images->chunk(5) as $key=>$image)
+                @foreach ($image as $item)
+                @if ($key===0)
+                @if (count($images) == 1)
+                <div class="">
+                    <div>
+                        <span class="image-block block2">
+                            <a class="" href="{{ asset('uploads/property/large/'.$item->path) }}"
+                                rel="prettyPhoto[gallery]">
+                                <img src="{{ asset('uploads/property/small/'.$item->path) }}" class="img-responsive"
+                                    alt="Gallery"></a>
                         </span>
                     </div>
-                    </div>
-                    @endif
-                    @endforeach
                 </div>
+                @elseif(count($images) == 2 || count($images) == 4 )
+                <div class="">
+                    <div>
+                        <span class="image-block block2">
+                            <a class="" href="{{ asset('uploads/property/large/'.$item->path) }}"
+                                rel="prettyPhoto[gallery]">
+                                <img src="{{ asset('uploads/property/small/'.$item->path) }}" class="img-responsive"
+                                    alt="Gallery"></a>
+                        </span>
+                    </div>
+                </div>
+                @elseif(count($images) == 3)
+                <div class="col-odd">
+                    <div>
+                        <span class="image-block block2">
+                            <a class="" href="{{ asset('uploads/property/large/'.$item->path) }}"
+                                rel="prettyPhoto[gallery]">
+                                <img src="{{ asset('uploads/property/small/'.$item->path) }}" class="img-responsive"
+                                    alt="Gallery"></a>
+                        </span>
+                    </div>
+                </div>
+                @else
+                <div class="gallery-img overflow-img">
+                    <a class="count_pics" href="#">+ {{ count($images) - 4 }}</a>
+                    <a class="" href="{{ asset('uploads/property/large/'.$item->path) }}" rel="prettyPhoto[gallery]">
+                        <img src="{{ asset('uploads/property/small/'.$item->path) }}" alt="Gallery">
+
+                    </a>
+
+
+                </div>
+                @endif
+                @endif
                 @endforeach
+                @endforeach
+                <!--
+                @foreach ($image as $item)
+                @if ($key===0)
+                <div class="col-md-3">
+                    <div>
+                        <span class="image-block block2">
+                            <a class="" href="{{ asset('uploads/property/large/'.$item->path) }}"
+                                rel="prettyPhoto[gallery]">
+                                <img src="{{ asset('uploads/property/small/'.$item->path) }}" class="img-responsive"
+                                    alt="Gallery"></a>
+                        </span>
+                    </div>
+                </div>
+                @else
+                <div class="">
+                    <div style="display: block">
+                        <span class="image-block block2">
+                            <a class="" href="{{ asset('uploads/property/large/'.$item->path) }}"
+                                rel="prettyPhoto[gallery]">
+                                <img src="{{ asset('uploads/property/small/'.$item->path) }}" class="img-responsive"
+                                    alt="Gallery"></a>
+                        </span>
+                    </div>
+                </div>
+
+                @endif
+                @endforeach -->
+
+
             </div>
-            <form action='' method = 'POST' class="container"> 
+            <form action='' method='POST' class="container">
                 @csrf
                 <div class="row">
                     <div class="col-10">
@@ -50,7 +109,8 @@
                             <thead class="bg-success">
                                 <th>Room Type</th>
                                 <th>Sleeps</th>
-                                <th>Price For <span class="text-light">{{ $totalstay }} {{ Str::plural('day',$totalstay) }}</span></th>
+                                <th>Price For <span class="text-light">{{ $totalstay }}
+                                        {{ Str::plural('day',$totalstay) }}</span></th>
                                 <th>Your Choices</th>
                                 <th>Select Rooms</th>
                             </thead>
@@ -64,19 +124,19 @@
                             <span class='total_cost'>0</span>.00
                         </p>
                         <p class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modal-info">
-                        Proceed to reserve
+                            Proceed to reserve
                         </p>
                     </div>
                 </div>
-                    <div class="modal fade" id="modal-info">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h4 class="modal-title">Personal Infomation</h4>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
+                <div class="modal fade" id="modal-info">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title">Personal Infomation</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
                             <div class="modal-body">
                                 <div class="form-group">
                                     <label for="name" class="form-label">Name</label>
@@ -91,7 +151,8 @@
                                     <input type="text" class="form-control" name="tel" required>
                                 </div>
                                 <div class="form-group">
-                                    <label for="message" class="form-label">What message would you like the hotel to know before you arrive?</label>
+                                    <label for="message" class="form-label">What message would you like the hotel to
+                                        know before you arrive?</label>
                                     <textarea class="form-control" name="message" id="" cols="30" rows="5"></textarea>
                                 </div>
                             </div>
@@ -108,26 +169,26 @@
             </form>
             <h3>Our facilities</h3>
             <ul>
-            <?php echo $facility_design; ?>
+                <?php echo $facility_design; ?>
             </ul>
         </div>
     </div>
-    </div>
-    <script>
-        $(document).ready(function(){
-            $(".quantity").on('change', function() {
-                var totalRoomCost = 0;
-                var numberOfRooms = 0;
-                //loop through each select
-                $(".quantity").each(function() {
-                    var roomNo = $(this).val() != "" ? parseInt($(this).val()) : 0;
-                    var cost = parseInt($(this).closest("tr").find('.charge p span').text());
-                    totalRoomCost += roomNo * cost;
-                    numberOfRooms += roomNo;
-                });
-                $('.room_no').text(numberOfRooms);
-                $('.total_cost').text(totalRoomCost);
-            });
-        })
-    </script>
+</div>
+<script>
+$(document).ready(function() {
+    $(".quantity").on('change', function() {
+        var totalRoomCost = 0;
+        var numberOfRooms = 0;
+        //loop through each select
+        $(".quantity").each(function() {
+            var roomNo = $(this).val() != "" ? parseInt($(this).val()) : 0;
+            var cost = parseInt($(this).closest("tr").find('.charge p span').text());
+            totalRoomCost += roomNo * cost;
+            numberOfRooms += roomNo;
+        });
+        $('.room_no').text(numberOfRooms);
+        $('.total_cost').text(totalRoomCost);
+    });
+})
+</script>
 @endsection
